3ca8605950c1f601d11e9c351628e514ca1d5170
ralleyne531 <ralleyne531@gmail.com>
2024-08-17 11:51:48 -0400

    refactored post and achievements routes. Removed test pages

--- ElevateEats/routes/achievements.js
+++ ElevateEats/routes/achievements.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router()
+router.use(express.json())
+const achievements = [
+    {
+        "id":1,
+        "name": "John",
+        "achievement_id":"1",
+        "timestamp":"050000"
+    }
+]
 
 router.post('/', (req,res,next) =>{
     console.log(req.body)
-    res.status(202).send('Suceess')
+    res.status(202).json(req.body)
 })
 
 module.exports = router;
--- ElevateEats/routes/posts.js
+++ ElevateEats/routes/posts.js
@@ -1,45 +1,63 @@
 const express = require('express')
 const router = express.Router()
-const posts = [{name:'John'}, {name:'Doe'}]
-
-router.get('/', (req, res) => {
-    res.send('POSTS LIST')
-  })
+router.use(express.json())
+const posts = [
+    {
+        "id": 0,
+        "user_id":123,
+        "content": "abc",
+        "image_url": "imgurl",
+        "timestamp": "025959"
+    },
+    {
+        "id": 1,
+        "user_id":456,
+        "content": "xyz",
+        "image_url": "imgurl1",
+        "timestamp": "030000"
+    }
+]
 
  //Static Routes
-router.get('/new', (req,res) => {
-    res.render('posts/new', { firstName: 'First' })
-})
-
+ //Create Post
 router.post('/', (req,res) => {
-    const isValid = true
-    if(isValid){
-        users.push({'name':req.body.firstName})
-        res.redirect(`/posts/${users.length -1}`)
-    } else{
-        console.log(error)
-        res.render('posts/new', {firstName: req.body.firstName})
+    if(Object.keys(req.body).length === 0){
+        console.log("There was an error creating this post, match type: JSON")
+        res.status(404).send('404 Bad Request')
+    }else{
+    console.log(req.body)
+    res.status(202).json(req.body)
     }
-    console.log(req.body.firstName)
 })
 
+//Get Posts
+router.get('/', (req, res) => {
+    res.send(posts)
+  })
+
+//Saving post information through middleware
 router.param('id', (req,res,next,id) => {
-    req.user = posts[id]
+    req.post = posts[id]
     next()
 })
 
-router.get('/:id', (req,res) =>{
-    console.log(req.user)
-    res.send(`Get User: ${req.user.name} With ID ${req.params.id}`)
-})
-
 router.post('/:id/comments', (req, res, next) => {
-    console.log(req)
-    res.status(202).send('Success')
+    if(Object.keys(req.body).length === 0){
+        console.log("There was an error creating this comment, match type: JSON")
+        res.status(404).send('404 Bad Request')
+    }else{
+    console.log(req.body)
+    res.status(202).json(req.body)
+    }
 })
 
 router.post('/:id/likes', (req, res, next) => {
-    console.log(req)
-    res.status(202).send('Success')
+    if(Object.keys(req.body).length === 0){
+        console.log("There was an error creating this comment, match type: JSON")
+        res.status(404).send('404 Bad Request')
+    }else{
+    console.log(req.body.likes)
+    res.status(202).json(req.body.likes)
+    }
 })
   module.exports = router
